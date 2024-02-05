110bf8f049f64a60b603180f34134051ad60a538
Sneh Saluja <snehsaluja@gmail.com>
2024-02-05 11:41:20 +0530

    Update LFUCache.java

--- src/main/java/com/practice/mypractice/interview4/cache/LFUCache.java
+++ src/main/java/com/practice/mypractice/interview4/cache/LFUCache.java
@@ -5,15 +5,17 @@ import java.util.Map;
 public class LFUCache implements Cache{
 
     public String get(int key) {
+        // write your implementation here
         return "";
     }
 
     public boolean put(int key, String value) {
+        // write your implementation here
         return true;
     }
 
     @Override
     public void display() {
-        //
+        // write your implementation here
     }
 }
