8087c2c1882aa0dec8f154af61cf0c1792870ff9
snehsaluja <snehsaluja@gmail.com>
2025-01-16 21:00:33 +0530

    Update RotateString.java

--- src/main/java/com/practice/mypractice/programs/RotateString.java
+++ src/main/java/com/practice/mypractice/programs/RotateString.java
@@ -37,6 +37,40 @@ public class RotateString {
         return leftRotate(s, len - d);
     }
 
+    public static String leftRotate_method2(String s, int d) {
+        String temp = s + s;
+        return temp.substring(d, d + s.length());
+    }
+
+    public static String rightRotate_method2(String s, int d) {
+        int len = s.length();
+
+        //return s.substring(len - d) + s.substring(0, len - d);
+        // OR :
+        return leftRotate_method2(s, len - d);
+    }
+
+    public static String leftRotate_method3(String s, int d) {
+        int len = s.length();
+        d = d % len;
+        char[] arr = s.toCharArray();
+        swap(arr, 0, d - 1);
+        swap(arr, d, len - 1);
+        swap(arr, 0, len - 1);
+        return new String(arr);
+    }
+
+    private static void swap(char[] arr, int start, int end) {
+        while (start < end) {
+            char temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+
     public static void main(String[] args) {
         String str1 = "GeeksforGeeks";
         System.out.println(leftRotate(str1, 2));
@@ -46,5 +80,10 @@ public class RotateString {
 
         System.out.println(rotateString(str2, "forGeeksGeeks"));
         System.out.println(rotateString(str2, "orGeeksGekesf"));
+
+        System.out.println(leftRotate_method2(str1, 2));
+        System.out.println(rightRotate_method2(str1, 2));
+
+        System.out.println(leftRotate_method3(str1, 2));
     }
 }
