931ebf9aaaa4d930e7c0d109c2fedece2c481425
snehsaluja <snehsaluja@gmail.com>
2025-01-16 20:46:07 +0530

    Update RotateString.java

--- src/main/java/com/practice/mypractice/programs/RotateString.java
+++ src/main/java/com/practice/mypractice/programs/RotateString.java
@@ -2,14 +2,14 @@ package com.practice.mypractice.programs;
 
 public class RotateString {
 
-    public boolean rotateString_bestSolution(String s, String goal) {
+    public static boolean rotateString_bestSolution(String s, String goal) {
         if (s.length() != goal.length())
             return false;
 
         return (s + s).contains(goal);
     }
 
-    public boolean rotateString(String s, String goal) {
+    public static boolean rotateString(String s, String goal) {
         if (s.length() != goal.length())
             return false;
         StringBuilder sb = new StringBuilder(goal);
@@ -25,7 +25,26 @@ public class RotateString {
         return false;
     }
 
-    public static void main(String[] args) {
+    public static String leftRotate(String s, int d) {
+        return s.substring(d) + s.substring(0, d);
+    }
 
+    public static String rightRotate(String s, int d) {
+        int len = s.length();
+
+        //return s.substring(len - d) + s.substring(0, len - d);
+        // OR :
+        return leftRotate(s, len - d);
+    }
+
+    public static void main(String[] args) {
+        String str1 = "GeeksforGeeks";
+        System.out.println(leftRotate(str1, 2));
+
+        String str2 = "GeeksforGeeks";
+        System.out.println(rightRotate(str2, 2));
+
+        System.out.println(rotateString(str2, "forGeeksGeeks"));
+        System.out.println(rotateString(str2, "orGeeksGekesf"));
     }
 }
